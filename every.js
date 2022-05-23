1b01d84d324174f9fdf95130845f4870db985bbc
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-05-23 21:58:21 +0400

    feat: add every.js

---
+++ every.js
@@ -0,0 +1,10 @@
+Array.prototype.customEvery = function (cb) {
+    for (let i = 0; i < this.length; i++) {
+      if (!cb(this[i], i, this)) {
+        return false
+      }
+    }
+  
+    return true;
+  };
+  
