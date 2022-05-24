4fc97aae5274f9e5df0c2b155962d9ab61e294da
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2022-05-24 21:10:37 +0400

    feat: add slice.js

--- slice.js
+++ slice.js
@@ -1,17 +1,17 @@
 Array.prototype.customSlice = function (startNum = 0, endNum = this.length) {
-    if (startNum < 0) {
-      startNum = this.length + startNum;
-    }
-
-    if (endNum < 0) {
-      endNum = this.length + endNum;
-    }
-
-    const slicedArr = [];
-    
-    for (let i = startNum; i < endNum; i++) {
-      slicedArr.push(arr[i]);
-    }
-    
-    return slicedArr;
+  if (startNum < 0) {
+    startNum = this.length + startNum;
   }
+
+  if (endNum < 0) {
+    endNum = this.length + endNum;
+  }
+
+  const slicedArr = [];
+
+  for (let i = startNum; i < endNum; i++) {
+    slicedArr[slicedArr[length]] = this[i];
+  }
+
+  return slicedArr;
+};
